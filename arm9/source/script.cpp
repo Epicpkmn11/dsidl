cc5b3a42ef82d4c6ded1220c6bd334ab73e8657f
Pk11 <epicpkmn11@outlook.com>
2022-06-16 10:07:38 -0500

    Add external script support
    A remote JSON file containing {"dsidl": 1, "script": []} with "script" being the same as a normal QR script

--- arm9/source/script.cpp
+++ arm9/source/script.cpp
@@ -6,6 +6,8 @@
 
 #include <nds.h>
 
+#define SCRIPT_VERSION 1
+
 static void downloadItem(std::string url, bool verbose) {
 	iprintf("================================");
 	iprintf("Download file?\nSource:\n%s\n\n(<A> Yes, <B> No)\n", url.c_str());
@@ -36,62 +38,97 @@ static void downloadItem(std::string url, bool verbose) {
 	}
 }
 
+static void runScriptInternal(const cJSON *json, bool verbose) {
+	if(cJSON_IsArray(json)) {
+		cJSON *item;
+		cJSON_ArrayForEach(item, json) {
+			if(cJSON_IsString(item)) {
+				if(strncmp(item->valuestring, "http", 4) == 0)
+					downloadItem(item->valuestring, verbose);
+				else
+					iprintf("================================%s\n", item->valuestring);
+			} else {
+				iprintf("Invalid JSON item.\n");
+				for(int i = 0; i < 60; i++)
+					swiWaitForVBlank();
+			}
+		}
+	} else if(cJSON_IsObject(json)) {
+		cJSON *item;
+		cJSON_ArrayForEach(item, json) {
+			if(cJSON_IsString(item)) {
+				if(strncmp(item->valuestring, "http", 4) == 0) {
+					iprintf("================================");
+					iprintf("Download %s?\nSource:\n%s\n\n(<A> Yes, <B> No)\n", item->string, item->valuestring);
+					u16 down;
+					do {
+						swiWaitForVBlank();
+						scanKeys();
+						down = keysDown();
+					} while(!(down & (KEY_A | KEY_B)));
+
+					if(down & KEY_A) {
+						int ret = download(item->valuestring, item->string, verbose);
+						if(ret >= 0)
+							iprintf("%s\ndownloaded successfully!\n", item->string);
+						else
+							iprintf("Download failed.\n");
+						for(int i = 0; i < 60; i++)
+							swiWaitForVBlank();
+					}
+				} else {
+					iprintf("================================%s\n", item->valuestring);
+				}
+			} else {
+				iprintf("Invalid JSON item:\n%s\n\n", item->string);
+				for(int i = 0; i < 60; i++)
+					swiWaitForVBlank();
+			}
+		}
+	} else {
+		iprintf("Invalid JSON.\n");
+		for(int i = 0; i < 60; i++)
+			swiWaitForVBlank();
+	}
+}
+
 void runScript(const std::string &payload, bool verbose) {
 	cJSON *json = cJSON_Parse(payload.c_str());
 	if(json) {
 		iprintf("Processing as JSON script...\n");
-		if(cJSON_IsArray(json)) {
-			cJSON *item;
-			cJSON_ArrayForEach(item, json) {
-				if(cJSON_IsString(item)) {
-					if(strncmp(item->valuestring, "http", 4) == 0)
-						downloadItem(item->valuestring, verbose);
-					else
-						iprintf("================================%s\n", item->valuestring);
-				} else {
-					iprintf("Invalid JSON item.\n");
-					for(int i = 0; i < 60; i++)
-						swiWaitForVBlank();
-				}
-			}
-		} else if(cJSON_IsObject(json)) {
-			cJSON *item;
-			cJSON_ArrayForEach(item, json) {
-				if(cJSON_IsString(item)) {
-					if(strncmp(item->valuestring, "http", 4) == 0) {
-						iprintf("================================");
-						iprintf("Download %s?\nSource:\n%s\n\n(<A> Yes, <B> No)\n", item->string, item->valuestring);
-						u16 down;
-						do {
-							swiWaitForVBlank();
-							scanKeys();
-							down = keysDown();
-						} while(!(down & (KEY_A | KEY_B)));
 
-						if(down & KEY_A) {
-							int ret = download(item->valuestring, item->string, verbose);
-							if(ret >= 0)
-								iprintf("%s\ndownloaded successfully!\n", item->string);
-							else
-								iprintf("Download failed.\n");
-							for(int i = 0; i < 60; i++)
-								swiWaitForVBlank();
+		// Check for external script reference
+		if(cJSON_IsObject(json)) {
+			cJSON *src = cJSON_GetObjectItemCaseSensitive(json, "dsidl:src");
+			if(src) {
+				if(cJSON_IsString(src) && strncmp(src->valuestring, "http", 4) == 0) {
+					char *jsonBuffer = (char *)malloc(1 << 20);
+					int ret = downloadBuffer(src->valuestring, jsonBuffer, 1 << 20, verbose);
+					if(ret == 0) {
+						cJSON_Delete(json);
+						json = cJSON_Parse(jsonBuffer);
+
+						cJSON *item = cJSON_GetObjectItemCaseSensitive(json, "dsidl");
+						if(item && cJSON_IsNumber(item) && item->valueint == SCRIPT_VERSION) {
+							item = cJSON_GetObjectItemCaseSensitive(json, "script");
+							if(item)
+								runScriptInternal(item, verbose);
 						}
-					} else {
-						iprintf("================================%s\n", item->valuestring);
 					}
+
+					free(jsonBuffer);
 				} else {
-					iprintf("Invalid JSON item:\n%s\n\n", item->string);
+					iprintf("Invalid script source.\n");
 					for(int i = 0; i < 60; i++)
 						swiWaitForVBlank();
 				}
+
+				cJSON_Delete(json);
+				return;
 			}
-		} else {
-			iprintf("Invalid JSON.\n");
-			for(int i = 0; i < 60; i++)
-				swiWaitForVBlank();
 		}
 
+		runScriptInternal(json, verbose);
 		cJSON_Delete(json);
 	} else if(payload.substr(0, 4) == "http") {
 		downloadItem(payload.c_str(), verbose);
